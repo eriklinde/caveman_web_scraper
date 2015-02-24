fe527d6192b75469da14c4c4a55b30ec986eb1fd
Erik Linde <erik.linde@gmail.com>
2015-02-23 23:05:02 -0500

    Changed filename

--- web.py
+++ web.py
@@ -7,21 +7,12 @@ app = Flask(__name__)
 def hello():
 	con = sqlite3.connect('nytimes.db')
 	results = con.execute("SELECT * FROM articles;")
-	# print("PRINTING SQL RESULTS!!!!")
-	# print(type(results))
-	# print(dir(results))
-	# print(results.fetchall())
-	# import pdb; pdb.set_trace()
-	# print("END OF PRINTING!!!")
-	# for result in results:
-		# print(result[1])
 	return render_template('articles.html', my_results=results)
 
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0')
 
 def extract_data():
-	print('hey')
 	con = sqlite3.connect('nytimes.db')
 	results = con.execute("SELECT * FROM articles;")
 
