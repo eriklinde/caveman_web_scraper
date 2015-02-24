fd2ed9e75f1df73aa0733208a2c8e5cc4941c676
Erik Linde <erik.linde@gmail.com>
2015-02-23 23:10:29 -0500

    Fixed DB path issue.

--- web.py
+++ web.py
@@ -7,14 +7,14 @@ DB_NAME = 'nytimes.db'
 
 @app.route("/")
 def index():
-        con = sqlite3.connect(os.path.dirname(os.path.realpath(__file__)) + "/" + DB_NAME)
-	results = con.execute("SELECT * FROM articles;")
-	return render_template('articles.html', my_results=results)
+    con = sqlite3.connect(os.path.dirname(os.path.realpath(__file__)) + "/" + DB_NAME)
+    results = con.execute("SELECT * FROM articles;")
+    return render_template('articles.html', my_results=results)
 
 if __name__ == "__main__":
-	app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
 
 def extract_data():
-        con = sqlite3.connect(os.path.dirname(os.path.realpath(__file__)) + "/" + DB_NAME)
-	results = con.execute("SELECT * FROM articles;")
+    con = sqlite3.connect(os.path.dirname(os.path.realpath(__file__)) + "/" + DB_NAME)
+    results = con.execute("SELECT * FROM articles;")
 
