51b7276553b9caba033be60a81cc340f453d15d8
Erik Linde <erik.linde@gmail.com>
2015-02-23 23:07:38 -0500

    Fixed DB path issue.

--- web.py
+++ web.py
@@ -2,6 +2,8 @@ import sqlite3
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
+import os
+DB_NAME = 'nytimes.db'
 
 @app.route("/")
 def hello():
@@ -13,6 +15,6 @@ if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0')
 
 def extract_data():
-	con = sqlite3.connect('nytimes.db')
+        con = sqlite3.connect(os.path.dirname(os.path.realpath(__file__)) + "/" + DB_NAME)
 	results = con.execute("SELECT * FROM articles;")
 
